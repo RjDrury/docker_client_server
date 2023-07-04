4f9fb5ee865564693200dec73696517152b4cbf8
Robert Drury <rob@Roberts-MacBook-Air.local>
2023-07-04 00:48:05 -0400

    Get it working to get breakpoints in vscode

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    const x = 1;
-    console.log(x);
-    return { message: 'Hello World from the man himself!' };
+    return { message: 'Hello World from the man himself!  ' };
   }
 }
